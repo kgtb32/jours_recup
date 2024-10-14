091f15e7b01f9f1e74ccc0d2fc21bb3d1cf5c08d
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2024-10-14 15:34:52 +0200

    Init spring boot application

---
+++ jours_recup/src/main/java/com/infotel/jours_recup/JoursRecupApplication.java
@@ -0,0 +1,13 @@
+package com.infotel.jours_recup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoursRecupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoursRecupApplication.class, args);
+	}
+
+}
---
+++ jours_recup/src/test/java/com/infotel/jours_recup/JoursRecupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infotel.jours_recup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoursRecupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
